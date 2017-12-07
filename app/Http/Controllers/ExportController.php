716afe3896d78412024fe0d683445ccd67e91df3
vagrant <vagrant@homestead>
2017-12-07 20:53:26 +0000

    Tweak export to csv

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -8,56 +8,79 @@ use Illuminate\Http\Request;
 class ExportController extends Controller
 {
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
 
-        // $this->middleware('subscribed');
+    // $this->middleware('subscribed');
+  }
+
+  public function csvExport(Book $book) 
+  {
+    $csv = \League\Csv\Writer::createFromFileObject(new \SplTempFileObject());
+
+    $csv->setOutputBOM(\League\Csv\Reader::BOM_UTF8);
+
+    $csv->insertOne([$this->getBookDetails($book)]);
+
+    $csv->insertOne(['Note', 'Date', 'Type']);
+
+    $notes = $book->notes()->get();
+    
+    foreach ($notes as $note) {
+      if ($note->type == 1) {
+        $noteType = 'Highlight';
+      }
+      
+      if ($note->type == 2) {
+        $noteType = 'Note';
+      }
+
+      $noteDate = date('d M Y, h:m:s',strtotime($note->date));
+
+      $csv->insertOne([$note->note, $noteDate, $noteType]);
     }
 
-    public function csvExport(Book $book) 
-    {
-        $csv = \League\Csv\Writer::createFromFileObject(new \SplTempFileObject());
-        $csv->setOutputBOM(\League\Csv\Reader::BOM_UTF8);
-        // Book details:
-        $details = $book->title_string;
-        if ($book->title !== '') {
-            $details = $book->title;
-            if ($book->author_first_name !== '' || $book->author_last_name !== '') {
-                $details .= ' (';
-            }
-            if ($book->author_first_name !== '') {
-                $details .= $book->author_first_name . ' ';
-            }
-            if ($book->author_last_name !== '') {
-                $details .= $book->author_last_name;
-            }
-            if ($book->author_first_name !== '' || $book->author_last_name !== '') {
-                $details .= ')';
-            }
-        }
+    $filename = 'notes.csv';
 
-        $csv->insertOne([$details]);
+    // https://github.com/barryvdh/laravel-debugbar/issues/621#issuecomment-288962915
 
-        // Notes:
-        $notes = $book->notes()->get();
-        $csv->insertOne(['Note', 'Date', 'Type']);
-        foreach ($notes as $note) {
-            if ($note->type == 1) {
-                $noteType = 'Highlight';
-            }
-            if ($note->type == 2) {
-                $noteType = 'Note';
-            }
-            $noteDate = date('d M Y, h:m:s',strtotime($note->date));
-            $csv->insertOne([$note->note, $noteDate, $noteType]);
-        }
+    return response((string) $csv, 200, [
+      'Content-Type' => 'text/csv',
+      'Content-Transfer-Encoding' => 'binary',
+      'Content-Disposition' => 'attachment; filename="' . $filename . '"'
+    ]);
+  }
 
-        $csv->output('notes.csv');
+  protected function getBookDetails($book)
+  {
+    if ($book->title == '') {
+      return $book->title_string;
     }
+
+    $details = $book->title;
+
+    if ($book->author_first_name !== '' || $book->author_last_name !== '') {
+      $details .= ' (';
+    }
+
+    if ($book->author_first_name !== '') {
+      $details .= $book->author_first_name . ' ';
+    }
+
+    if ($book->author_last_name !== '') {
+      $details .= $book->author_last_name;
+    }
+
+    if ($book->author_first_name !== '' || $book->author_last_name !== '') {
+      $details .= ')';
+    }
+    
+    return $details;
+  }
 }
