09173eb4b9930e89cf767a84abb0373d6502e976
vagrant <vagrant@homestead>
2018-01-03 21:55:50 +0000

    Disable profile image upload

--- resources/views/vendor/spark/settings/profile.blade.php
+++ resources/views/vendor/spark/settings/profile.blade.php
@@ -1,8 +1,5 @@
 <spark-profile :user="user" inline-template>
     <div>
-        <!-- Update Profile Photo -->
-        @include('spark::settings.profile.update-profile-photo')
-
         <!-- Update Contact Information -->
         @include('spark::settings.profile.update-contact-information')
     </div>
